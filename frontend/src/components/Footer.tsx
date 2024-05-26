2e8e50c487ba76e234d30721e65351bebaaf7187
Krishnan S <skrishnan2001@gmail.com>
2024-05-26 22:38:51 +0530

    feat: Completed the backend and frontend of Plinko Alchemy

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { FaGithub, FaTwitter, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <footer className="border-t border-gray-600 py-12 text-white">
+      <div className="w-[96%] max-w-screen-lg mx-auto flex flex-row justify-between">
+        <div className="flex items-center">
+          <Link
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            {/* <img
+            src="https://res.cloudinary.com/dcugqfvvg/image/upload/v1713647295/standardboard.1d6f9426_asqzum.png"
+            className="h-8"
+            alt="plinkoo Logo"
+          /> */}
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Plinko Alchemy
+            </span>
+          </Link>
+        </div>
+        <div>
+          <div className="space-y-2">
+            <h1 className="text-center text-lg">Follow On</h1>
+            <div className="flex items-center gap-3">
+              <a href="https://github.com/skrishnan2001" target="_blank">
+                <FaGithub size={30} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
